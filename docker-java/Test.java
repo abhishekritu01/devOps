6f277b04553a3d8aa41094d56aef5c82b138c100
abhishek <abhishekritu810219@gmail.com>
2024-04-13 11:38:56 +0530

    test

---
+++ docker-java/Test.java
@@ -0,0 +1,19 @@
+import java.util.Properties;
+
+/**
+ * Test
+ */
+public class Test {
+
+    // public static void printSomething() {
+    //     System.out.println("Hello World");
+
+    //     Properties properties = System.getProperties();
+    //     System.out.println(properties);
+    // }
+
+    public static void main(String[] args) {
+        System.out.println("Java programming is fun   ===================================");
+        // printSomething();
+    }
+}
---
+++ express-server/index.js
@@ -0,0 +1,22 @@
+
+// express server
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// body parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// routes
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to the application.' });
+});
+
+app.listen(port, () => {
+
+    console.log(`Server is running on port ${port}`);
+}   
+);
